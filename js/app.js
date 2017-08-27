e1d7c99d7ff32e5c31fef89e1260052d3a5e6170
RAVi <expertravi2008@gmail.com>
2017-08-27 15:18:02 +0530

    Added basic ui-router functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,43 @@
 var app=angular.module('app',['ui.router']);
 
-app.config(function($stateProvider,$urlRouterProvider){
+app.config(function($stateProvider,$urlRouterProvider,$locationProvider){
 	$urlRouterProvider.otherwise('/');
 	$stateProvider
-		.state('/',{
+		.state('root',{
 			url:'/',
-			templateUrl:'index.html',
-			controller:'indexController'
+			templateUrl:'html/main.html'
 		})
-		.state('home',{
-			url:'/home',
-			templateUrl:'home.html'
-		})
+		.state('about',{
+			url:'/about',
+			templateUrl:'html/about.html',
+			controller:'aboutController'
+		});
+
+	//send to home is route is not found
+	$urlRouterProvider.otherwise('/');
+
+	//enable html5 mode url rewriting
+	/*$locationProvider.html5Mode({
+		enabled:true,
+		requireBase:true
+	})*/
+});
+
+
+app.run(($rootScope)=>{
+	$rootScope.appname='AppName';
+	$rootScope.navlinks=[
+		{
+			text:'Home',
+			href:'root'
+		},
+		{
+			text:'About',
+			href:'about'
+		}
+	];
+});
+
+app.controller('aboutController',($scope)=>{
+	$scope.text='Some text from controller';
 });
